9b920da9d2e96cbd6c6c77ca72baed718c4154b5
sherncrln <sherncrln@gmail.com>
2024-06-20 22:27:18 +0700

    updating ClassDetail.js -edit

--- src/pages/ClassDetail.js
+++ src/pages/ClassDetail.js
@@ -40,126 +40,128 @@ export default function ClassDetail() {
     
     const handleSubmit = (event)=>{
         event.preventDefault();
-        // axios.put(`http://localhost/timetofill/users.php/${id}`, JSON.stringify(classData))
-        //     .then(function(response){
-        //         console.log(response.data);
-        //         setClassData(response.data);
-        //         // backToClassPage();
-        //     })
-        //     .catch(function(error){
-        //         console.error("There was an error!", error);
-        //     });
+        console.log(classData);
+        axios.put(`http://localhost/timetofill/class.php/${id}`, JSON.stringify(classData))
+            .then(function(response){
+                setClassData(response.data);
+                backToClassPage();
+            })
+            .catch(function(error){
+                console.error("There was an error!", error);
+            });
     }
 
   return (
     <>
-      <div>
+      <div className="min-h-screen flex flex-col">
         <NavBar />
         <p className="text-5xl text-blue-800 text-center tracking-widest mt-14 mb-6">Class Detail</p>
-        <div className="w-full flex justify-center">
-          <form className="flex justify-center mb-12" >
-          <div className="w-1/2 flex mr-4">
-              <div className= "w-72 mb-2">
-                <label
-                    className="text-sm text-blue-950 text-bold font-semibold">
-                    Class
-                </label>
-                <input
-                    type="text"
-                    className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
-                    id="class"
-                    name="class"
-                    placeholder=""
-                    value={classData.class}
-                    onChange={handleChange}
-                />
-                <label
-                    className="text-sm text-blue-950 text-bold font-semibold">
-                    Category
-                </label>
-                <input
-                    type="text"
-                    className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
-                    id="category"
-                    name="category"
-                    placeholder=""
-                    value={classData.category}
-                    onChange={handleChange}
-                />
-                <label
-                    className="text-sm text-blue-950 text-bold font-semibold">
-                    Semester
-                </label>
-                <input
-                    type="number"
-                    className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
-                    id="semester"
-                    name="semester"
-                    placeholder=""
-                    min="1" max="8"
-                    value={classData.semester}
-                    onChange={handleChange}
-                />
-                <label
-                    className="text-sm text-blue-950 text-bold font-semibold">
-                    Valid From
-                </label>
-                <input
-                    type="date"
-                    className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
-                    id="valid_from"
-                    name="valid_from"
-                    placeholder=""
-                    value={classData.valid_from}
-                    onChange={handleChange}
-                />
-            
-                <label
-                    className="text-sm text-blue-950 text-bold font-semibold">
-                    Valid To
-                </label>
-                <input
-                    type="date"
-                    className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
-                    id="valid_to"
-                    name="valid_to"
-                    placeholder=""
-                    value={classData.valid_to}
-                    onChange={handleChange}
-                />
-              </div>
-          </div>
-          <div className="w-1/2 flex ml-2">
-            <div className= "w-72 mb-2 ">
-                {Array.from({ length: 6 }, (_, index) => (
-                    <div key={index}>
-                        <label className="text-sm text-blue-950 text-bold font-semibold">
-                            Variable {index + 1}
-                        </label>
-                        <select
-                            id={`variable_${index + 1}`}
-                            name={`variable_${index + 1}`}
-                            className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
-                            value={classData[`variable_${index + 1}`] || ''}
-                            onChange={handleChange}
-                        >
-                            <option value="">Select Dosen</option>
-                            {dosen.map((d, id) => (
-                                <option key={id} value={d.username}>
-                                    {d.name}
-                                </option>
-                            ))}
-                        </select>
+        <form className="flex justify-center mb-12" onSubmit={handleSubmit} >
+        <div className="flex-row">
+            <div className="w-full flex justify-center">
+                <div className="w-1/2 flex mr-4">
+                    <div className= "w-72 mb-2">
+                    <label
+                        className="text-sm text-blue-950 text-bold font-semibold">
+                        Class
+                    </label>
+                    <input
+                        type="text"
+                        className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
+                        id="class"
+                        name="class"
+                        placeholder=""
+                        value={classData.class}
+                        onChange={handleChange}
+                    />
+                    <label
+                        className="text-sm text-blue-950 text-bold font-semibold">
+                        Category
+                    </label>
+                    <input
+                        type="text"
+                        className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
+                        id="category"
+                        name="category"
+                        placeholder=""
+                        value={classData.category}
+                        onChange={handleChange}
+                    />
+                    <label
+                        className="text-sm text-blue-950 text-bold font-semibold">
+                        Semester
+                    </label>
+                    <input
+                        type="number"
+                        className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
+                        id="semester"
+                        name="semester"
+                        placeholder=""
+                        min="1" max="8"
+                        value={classData.semester}
+                        onChange={handleChange}
+                    />
+                    <label
+                        className="text-sm text-blue-950 text-bold font-semibold">
+                        Valid From
+                    </label>
+                    <input
+                        type="date"
+                        className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
+                        id="valid_from"
+                        name="valid_from"
+                        placeholder=""
+                        value={classData.valid_from}
+                        onChange={handleChange}
+                    />
+                
+                    <label
+                        className="text-sm text-blue-950 text-bold font-semibold">
+                        Valid To
+                    </label>
+                    <input
+                        type="date"
+                        className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
+                        id="valid_to"
+                        name="valid_to"
+                        placeholder=""
+                        value={classData.valid_to}
+                        onChange={handleChange}
+                    />
                     </div>
-                ))}
-            </div>
-          </div>
-          </form>
-      </div >
-      <div className= " w-full flex items-center justify-center mb-2 px-4 tracking-widest text-[#f8fafc]">
-          <button className="w-28 h-8 rounded bg-[#577BC1] mr-6 ">Save</button>
-          <button onClick={backToClassPage} className="w-32 h-8 rounded bg-[#577BC1] ">Back</button>
-      </div>
+                </div>
+                <div className="w-1/2 flex ml-2">
+                <div className= "w-72 mb-2 ">
+                    {Array.from({ length: 6 }, (_, index) => (
+                        <div key={index}>
+                            <label className="text-sm text-blue-950 text-bold font-semibold">
+                                Variable {index + 1}
+                            </label>
+                            <select
+                                id={`variable_${index + 1}`}
+                                name={`variable_${index + 1}`}
+                                className="w-72 h-10 px-4 peer py-2 text-blue-900 bg-transparent border border-blue-950 rounded"
+                                value={classData[`variable_${index + 1}`] || ''}
+                                onChange={handleChange}
+                            >
+                                <option value="">Select Dosen</option>
+                                {dosen.map((d, id) => (
+                                    <option key={id} value={d.username}>
+                                        {d.name}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    ))}
+                </div>
+                </div>
+            </div >
+        <div className= "w-full flex items-center justify-center mt-8 mb-2 px-4 tracking-widest text-[#f8fafc]">
+            <button type="submit" className="w-28 h-8 rounded bg-[#577BC1] mr-6 ">Save</button>
+            <button type="button" onClick={backToClassPage} className="w-32 h-8 rounded bg-[#577BC1] ">Back</button>
+        </div>
+        </div >
+        </form>
       </div>
     </>
 );
